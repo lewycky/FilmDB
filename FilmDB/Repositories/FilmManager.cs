179727ba975c34ef6e5d48950dec46bb169cf44f
Arek Lewicki <lewicki1arek@gmail.com>
2023-04-02 11:17:32 +0200

    First step in application

--- FilmDB/Repositories/FilmManager.cs
+++ FilmDB/Repositories/FilmManager.cs
@@ -1,6 +1,37 @@
-﻿namespace FilmDB.Repositories
+﻿using FilmDB.Models;
+
+namespace FilmDB.Repositories
 {
     public class FilmManager
     {
+        public FilmManager AddFilm(FilmModel filmModel)
+        {
+            return this;
+        }
+
+        public FilmManager RemoveFilm(int id)
+        {
+            return this;
+        }
+
+        public FilmManager UpdateFilm(FilmModel filmModel)
+        {
+            return this;
+        }
+
+        public FilmManager ChangeTitle(int id, string newTitle)
+        {
+            return this;
+        }
+
+        public FilmManager GetFilm(int id)
+        {
+            return null;
+        }
+
+        public List<FilmModel> GetFilms()
+        {
+            return null;
+        }
     }
 }
